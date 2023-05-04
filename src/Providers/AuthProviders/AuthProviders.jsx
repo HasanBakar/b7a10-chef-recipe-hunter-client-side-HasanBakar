e41b3d8dec1953b1cbff8d4f67bec9f75fc55ac7
Abu Bakar <abubakar.hasan.cse@gmail.com>
2023-05-04 16:08:17 +0600

    authentication system integrate with github added

--- src/Providers/AuthProviders/AuthProviders.jsx
+++ src/Providers/AuthProviders/AuthProviders.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from "react";
-import { getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword,onAuthStateChanged ,signOut,GoogleAuthProvider,signInWithPopup } from "firebase/auth";
+import { getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword,onAuthStateChanged ,signOut,GoogleAuthProvider,signInWithPopup,GithubAuthProvider  } from "firebase/auth";
 
 
 import { createContext } from "react";
@@ -9,25 +9,33 @@ import app from './../../firebase/firebase.config';
 const auth = getAuth(app);
 const GoogleProvider = new GoogleAuthProvider();
 
+const githubProvider = new GithubAuthProvider();
+
+
 export const AuthContext = createContext(null)
 
 const AuthProviders = ({children}) => {
     const [user , setUser] = useState(null)
     const [loading, setLoading] = useState(true)
 
-
+/**handle email/password created user */
 const createUser = (email, password) =>{
     setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password)
 }
-
+/** handle signIn with email and password */
 const signIn = (email, password)=>{
     setLoading(true)
     return signInWithEmailAndPassword(auth, email, password)
 }
 
+/**handle with google signIn */
 const handleWithGoogle = () =>{
     return signInWithPopup(auth, GoogleProvider)
+}
+
+const handleWithGithub = () =>{
+    return signInWithPopup(auth, githubProvider)
 }
 
 const logOut = () =>{
@@ -50,7 +58,7 @@ const logOut = () =>{
     const authInfo ={
         createUser,loading,
         user,signIn,setUser,
-        logOut,handleWithGoogle
+        logOut,handleWithGoogle,handleWithGithub
     }
     return (
         <AuthContext.Provider value={authInfo}>
